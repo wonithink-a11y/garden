abd51547aaba6ecc14cdb4789314e0e29118865d
wonithink-a11y <wonithink@gmail.com>
2026-06-01 22:34:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
-const CACHE_NAME = 'garden-app-v54';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'garden-app-v55';
+const BASE = '/garden';
+const urlsToCache = [BASE+'/', BASE+'/index.html', BASE+'/manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache).catch(()=>{})));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
   ));
+  self.clients.claim();
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
